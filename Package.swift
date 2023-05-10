116b3cbf3d9bd0f20811ea9481e1431ce1159009
Reza Ali <syed.reza.ali@gmail.com>
2023-05-10 10:59:09 -0700

    modified Package.swift to track Satin & Easings master branches

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "Juicer", targets: ["Juicer"])],
     dependencies: [
-        .package(url: "https://github.com/Hi-Rez/Satin", from: "2.1.0"),
-        .package(url: "https://github.com/Hi-Rez/Easings", from: "0.0.4")
+        .package(url: "https://github.com/Hi-Rez/Satin", .branch("master")),
+        .package(url: "https://github.com/Hi-Rez/Easings", .branch("master"))
     ],
     targets: [.target(name: "Juicer", dependencies: ["Satin", "Easings"])],
     swiftLanguageVersions: [.v5]
